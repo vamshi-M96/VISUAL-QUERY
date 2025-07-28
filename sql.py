2f57aedfb517390599206666945746be19f2bc9b
vamshi-M96 <vamsi.meka96@gmail.com>
2025-07-29 00:30:54 +0530

    Update sql.py

--- sql.py
+++ sql.py
@@ -30,7 +30,7 @@ st.markdown(
 
     .wave-text span {
         display: inline-block;
-        animation: wave 0.8s ease-in-out infinite;
+        animation: wave 3.8s ease-in-out infinite;
     }
 
     .reflection {
